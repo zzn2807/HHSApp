603be5c5e00df8c4c29b818d087a3f829d17455c
David Nwokolo <nwokolodz@gmail.com>
2025-07-22 13:10:14 -0400

    Added ability to swipe to open and close menu

--- src/App.js
+++ src/App.js
@@ -3,17 +3,43 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Menu from './Components/Menu';
+import MenuContext from './Context/MenuContext';
 import { Route, Routes } from 'react-router-dom';
-
+import { useState, useContext } from 'react';
 function App() {
+  const { setMenuOpen } = useContext(MenuContext);
+  const [touchStart, setTouchStart] = useState(null)
+  const [touchEnd, setTouchEnd] = useState(null)
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 50
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX)
+  }
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX)
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return
+    const distance = touchStart - touchEnd
+    const isLeftSwipe = distance > minSwipeDistance
+    const isRightSwipe = distance < -minSwipeDistance
+    // if (isLeftSwipe || isRightSwipe) console.log('swipe', isLeftSwipe ? 'left' : 'right')
+    // add your conditional logic here
+    if (isRightSwipe) {
+      setMenuOpen(true);
+    }
+  }
   return (
-    <div className="App">
-      <Menu/>
-      <Header/>
+    <div className="App" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
+      <Menu />
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,15 +1,38 @@
 import { NavLink } from "react-router-dom";
 import "../css/Menu.css"
-import { useContext, useEffect } from "react";
+import { useContext, useState } from "react";
 import MenuContext from "../Context/MenuContext";
-function Menu(){
-    const {setMenuOpen} = useContext(MenuContext);
-    
+function Menu() {
+    const { setMenuOpen } = useContext(MenuContext);
+    const [touchStart, setTouchStart] = useState(null)
+    const [touchEnd, setTouchEnd] = useState(null)
+
+    // the required distance between touchStart and touchEnd to be detected as a swipe
+    const minSwipeDistance = 50
+
+    const onTouchStart = (e) => {
+        setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+        setTouchStart(e.targetTouches[0].clientX)
+    }
+
+    const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX)
+
+    const onTouchEnd = () => {
+        if (!touchStart || !touchEnd) return
+        const distance = touchStart - touchEnd
+        const isLeftSwipe = distance > minSwipeDistance
+        const isRightSwipe = distance < -minSwipeDistance
+        // if (isLeftSwipe || isRightSwipe) console.log('swipe', isLeftSwipe ? 'left' : 'right')
+        // add your conditional logic here
+        if(isLeftSwipe){
+            setMenuOpen(false);
+        }
+    }
     return (
-        <div className={`menu`}>
+        <div className="menu" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
             <div className="menu-header">
                 <h1 className="menu-title">Menu</h1>
-                <h1 className="menu-x" onClick={()=>setMenuOpen(false)}>&#x2716;</h1>
+                <h1 className="menu-x" onClick={() => setMenuOpen(false)}>&#x2716;</h1>
             </div>
             <ul className="menu-list">
                 <li className="menu-item"><NavLink to="/" end>Home</NavLink></li>
