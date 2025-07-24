58d159e31cf6cd8f97fa9310aaf6605b265b85cb
David Nwokolo <nwokolodz@gmail.com>
2025-07-24 09:45:16 -0400

    Fleshed out survey and miscellaneous pages

--- src/Components/Misc.js
+++ src/Components/Misc.js
@@ -1,9 +1,22 @@
-
+import phInfoImg from "../img/ph-info.jpg"
 import "../css/Page.css"
 function Misc() {
     return (
         <div className="content">
-            <h1>Welcome to Hope Health Systems!</h1>
+            <h1>Public Health Info</h1>
+            <ul className="content-ul">
+                <li>Lorem ipsum dolor sit amet, consectetur</li>
+                <li>adipiscing elit. Nam ligula leo, molestie sed est </li>
+                <li>sed, mollis dictum sapien. Donec sit amet ligula </li>
+                <li>consequat, facilisis mi eget, gravida dolor. Nunc t</li>
+                <li>empor leo massa, quis porta nisl accumsan non. </li>
+                <li>Cras eget nisi lacus. Curabitur lectus massa, </li>
+            </ul>
+            <img src={phInfoImg} className="content-img"/>
+            <p>
+                fermentum id nisi a, pulvinar bibendum ipsum. Phasellus et semper neque. Proin tincidunt sollicitudin augue eu bibendum. Sed quis sollicitudin tellus, vel tincidunt tortor. Pellentesque elit nulla, luctus quis sagittis ac, faucibus in justo. Sed et tempor risus. Quisque sem sapien, facilisis a pulvinar at, rutrum et purus.
+            </p>
+            <a className="btn">Click for more info</a>
         </div>
     )
 }
--- src/Components/Survey.js
+++ src/Components/Survey.js
@@ -3,7 +3,31 @@ import "../css/Page.css"
 function Survey() {
     return (
         <div className="content">
-            <h1>Welcome to Hope Health Systems!</h1>
+            <h1>Client Satisfaction Survey</h1>
+            <form>
+                <div className="form-div">
+                    <label>Question 1</label>
+                    <input type="text"></input>
+                </div>
+                <div className="form-div">
+                    <label>Question 2</label>
+                    <input type="text"></input>
+                </div>
+                <div className="form-div">
+                    <label>Question 3</label>
+                    <input type="date"></input>
+                </div>
+                <div className="form-div flex-row">
+                    <label>Question 4</label>
+                    <input type="checkbox"></input>
+                    <input type="checkbox"></input>
+                    <input type="checkbox"></input>
+                </div>
+                <div className="form-div">
+                    <input type="file"></input>
+                </div>
+                <button className="form-submit">Submit</button>
+            </form>
         </div>
     )
 }
